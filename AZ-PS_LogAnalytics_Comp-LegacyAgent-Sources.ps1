b4184c7235ce9702ceaa8daa468e55e8a74342a5
rdill-83 <57857105+rdill-83@users.noreply.github.com>
2023-10-09 23:37:50 -0700

    Update AZ-PS_LogAnalytics_Comp-LegacyAgent-Sources.ps1

--- AZ-PS_LogAnalytics_Comp-LegacyAgent-Sources.ps1
+++ AZ-PS_LogAnalytics_Comp-LegacyAgent-Sources.ps1
@@ -1,4 +1,4 @@
-# AZ-PS_LogAnalytics_Comp-Sources
+# AZ-PS_LogAnalytics_Comp-LegacyAgent-Sources
 
 # RG To Variable:
 $RG = (Get-AZResourceGroup -ResourceGroupName rg-diag).ResourceGroupName
@@ -6,13 +6,13 @@ $RG = (Get-AZResourceGroup -ResourceGroupName rg-diag).ResourceGroupName
 # Log Analytics Space to Variable:
 $LA = (Get-AZOperationalInsightsWorkspace -ResourceGroupName $RG).ResourceID
 
-# KQL Query to Variable (all Comps w/ Legacy LogAnalytics Agent Installed)
+# KQL Query to Variable (all Comps w/ Legacy LogAnalytics Agent Installed):
 $query = "Heartbeat | where OSType == 'Windows' | where Category != 'Azure Monitor Agent' | summarize arg_max(TimeGenerated, *) by SourceComputerId | sort by Computer | render table"
 
 # Invoke Log Analytics KQL Query:
 $AZLAComps = Invoke-AZOperationalInsightsQuery -WorkspaceID $LA -Query $query
 
-# Generate Query Results - Full
+# Generate Query Results - Full:
 $AZLAComps.Results
 
 # Generate Query Results - Pertinent Info:
